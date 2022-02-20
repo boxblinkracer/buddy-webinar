75833b309178e0f43d7b50fa0b65aa043dab19cf
boxblinkracer <boxblinkracer@gmx.net>
2022-02-20 22:34:43 +0100

    add dockware sample plugin

---
+++ shop/src/custom/plugins/DockwareSamplePlugin/src/Subscriber/Storefront/Subscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DockwareSamplePlugin\Subscriber\Storefront;
+
+use Shopware\Storefront\Event\StorefrontRenderEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class Subscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            StorefrontRenderEvent::class => 'onStorefrontRender',
+        ];
+    }
+
+    /**
+     * @param StorefrontRenderEvent $event
+     */
+    public function onStorefrontRender(StorefrontRenderEvent $event)
+    {
+        echo "Buddy was here!";
+    }
+
+}
